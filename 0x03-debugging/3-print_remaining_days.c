8a0c46d556fb83cc8920beaccdc6d9382429f533
drissdevlpr <driss.abr@gmail.com>
2023-03-28 20:11:49 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,36 +1,14 @@
-#include <stdio.h>
-#include "main.h"
+On branch master
+Your branch is up to date with 'origin/master'.
 
-/**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	modified:   3-print_remaining_days.c
+	modified:   ../README.md
 
-void print_remaining_days(int month, int day, int year)
-{
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-	{
-		if (month >= 2 && day >= 60)
-		{
-			day++;
-		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
-	else
-	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
-}
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	../0x01-variables_if_else_while/102-print_comb5
+
+no changes added to commit (use "git add" and/or "git commit -a")
